8dc9f8c4e1b07f4e160baeaacc9d3e3faef6feda
solpie <solpie.net@gmail.com>
2013-05-23 19:24:18 +0800

    anydbm

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,28 @@ import os
 import sys
 
 sys.path.insert(0, os.path.join('.', 'site-packages'))
+###
+import anydbm
+
+# Open database, creating it if necessary.
+db = anydbm.open('cache', 'c')
+
+# Record some values
+db['www.python.org'] = 'Python Website'
+db['www.cnn.com'] = 'Cable News Network'
+
+# Loop through contents.  Other dictionary methods
+# such as .keys(), .values() also work.
+for k, v in db.iteritems():
+    print k, '\t', v
+
+# Storing a non-string key or value will raise an exception (most
+# likely a TypeError).
+db['www.yahoo.com'] = 4
+
+# Close when done.
+db.close()
+###
 from leechMVP import app
 
 application = app
